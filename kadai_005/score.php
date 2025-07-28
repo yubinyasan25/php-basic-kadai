5bf988a484f7509876e9b823a1a90c7a0d619c48
yubinyasan25 <yubin.yasan.27@gmail.com>
2025-07-28 15:43:47 +0900

    課題

---
+++ kadai_005/score.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>テストの平均</title>
+</head>
+
+<body>
+   <p>
+       <?php
+    
+       $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+
+        echo '合計'.$score1+$score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10 ,'点';
+
+         echo "<br>";
+        
+
+      echo '平均' .($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) / 10 ,'点';
+            ?>
+   </p>
+</body>
+
+</html>
