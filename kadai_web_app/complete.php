cb11e33b35da8ddf696b5b239d70c52364b07979
yubinyasan25 <yubin.yasan.27@gmail.com>
2025-08-18 16:45:55 +0900

    Add files via upload
    課題提出

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <h2>お問い合わせを承りました。</h2>
+    <p>ありがとうございました。今後の参考にさせていただきます。</p>
+    <button id="back-btn" onclick="location.href='form.php';">戻る</button>
+</body>
+
+</html>
